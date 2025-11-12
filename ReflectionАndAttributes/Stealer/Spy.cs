b800431d1244be6e95d11cd045799fb2783c9f1f
Ivan Pehsterski <peshterskiivan@gmail.com>
2025-11-12 06:43:39 +0200

    Add files via upload

---
+++ ReflectionАndAttributes/Stealer/Spy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+	public class Spy
+	{
+		public string StealFieldInfo(string className, params string[] fieldName)
+		{
+			Console.WriteLine($"Class under investigation: {className}");
+
+			var type = Type.GetType(className);
+
+			var fields = type.GetFields(BindingFlags.Instance
+				| BindingFlags.NonPublic
+				| BindingFlags.Public);
+
+			var searched = fields
+				.Where(f => fieldName.Contains(f.Name))
+				.ToList();
+
+			var instance = Activator.CreateInstance(type);
+
+			var result = new StringBuilder();
+
+			foreach (var field in searched)
+			{
+				var value = field.GetValue(instance);
+
+				result.AppendLine($"{field.Name} = {value}");
+			}
+
+			return result.ToString();
+		}
+
+    }
+}
+
---
+++ ReflectionАndAttributes/Stealer/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Stealer
+{
+	public class StartUp
+	{
+		public static void Main()
+		{
+			var spy = new Spy();
+
+			string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+
+			Console.WriteLine(result);
+		}
+	}
+}
+
