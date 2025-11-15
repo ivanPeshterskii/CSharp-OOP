489716c6c62513f7c7a7955da2668ce70296918b
Ivan Pehsterski <peshterskiivan@gmail.com>
2025-11-15 16:24:45 +0200

    Add files via upload

---
+++ UnitTestng/Skeleton.Tests/AxeTests.cs
@@ -0,0 +1,42 @@
+using System;
+using NUnit.Framework;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class AxeTests
+    {
+        [Test]
+        public void AxeShouldLooseDurabilityAfterAttack()
+        {
+            // Arrange
+            var axe = new Axe(10, 10);
+            var dummy = new Dummy(10, 10);
+
+            // Act
+            axe.Attack(dummy);
+
+            // Assert
+            Assert.That(axe.DurabilityPoints, Is.EqualTo(9));
+        }
+
+        [Test]
+        public void AxeShouldNotAttackWhenBroken()
+        {
+            // Arrange
+            var axe = new Axe(10, -5);
+            var dummy = new Dummy(100, 200);
+
+            // Act
+            axe.Attack(dummy);
+
+            // Assert
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                // Act
+                axe.Attack(dummy);
+
+            }, "Axe is broken.");
+        }
+    }
+}
---
+++ UnitTestng/Skeleton.Tests/DummyTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        [Test]
+        public void DummyLosesHealthIfAttacked()
+        {
+            // Arrange
+            var axe = new Axe(10, 100);
+            var dummy = new Dummy(100, 200);
+
+            // Act
+            axe.Attack(dummy);
+
+            // Assert
+            Assert.That(dummy.Health, Is.EqualTo(90));
+        }
+
+        [Test]
+        public void DummyShouldNotBeAttackedWhenDead()
+        {
+            var axe = new Axe(10, 100);
+            var dummy = new Dummy(0,200);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                axe.Attack(dummy);
+
+            }, "Dummy is dead!");
+        }
+
+        [Test]
+        public void DeadDummyCanGiveXp()
+        {
+            var dummy = new Dummy(0, 200);
+
+            var result = dummy.GiveExperience();
+
+            Assert.That(result, Is.EqualTo(200));
+        }
+
+        [Test]
+        public void AliveDummyCannotGiveEx()
+        {
+            var dummy = new Dummy(100, 200);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                var result = dummy.GiveExperience();
+            }, "Dummy can't give ex when alive!");
+        }
+    }
+}
