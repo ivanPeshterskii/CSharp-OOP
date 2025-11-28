61427c229df975c3768e4d6563f37725641af2c8
Ivan Pehsterski <peshterskiivan@gmail.com>
2025-11-28 13:33:34 +0200

    Add files via upload

---
+++ Championship/Models/Manager.cs
@@ -0,0 +1,38 @@
+﻿using FootballManager.Models.Contracts;
+using FootballManager.Utilities.Messages;
+
+namespace FootballManager.Models
+{
+    public abstract class Manager : IManager
+    {
+        private string name;
+        private double ranking;
+
+        protected Manager(string name, double ranking)
+        {
+            Name = name;
+            Ranking = ranking;
+        }
+
+        public string Name 
+        { 
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.ManagerNameNull);
+                }
+                name = value;
+            }
+        }
+
+        public double Ranking { get => ranking; protected set => ranking = value; }
+
+        public abstract void RankingUpdate(double updateValue);
+        public override string ToString()
+        {
+            return $"{this.Name} - {this.GetType().Name} (Ranking: {this.Ranking:F2})";
+        }
+    }
+}
---
+++ Championship/Repositories/TeamRepository.cs
@@ -0,0 +1,49 @@
+﻿using FootballManager.Models.Contracts;
+using FootballManager.Repositories.Contracts;
+
+namespace FootballManager.Repositories
+{
+    public class TeamRepository : IRepository<ITeam>
+    {
+        private readonly List<ITeam> models;
+        private int capacity = 10;
+
+        public TeamRepository()
+        {
+            this.models = new List<ITeam>();
+
+        }
+        public IReadOnlyCollection<ITeam> Models => this.models.AsReadOnly();
+
+        public int Capacity => this.capacity;
+
+        public void Add(ITeam model)
+        {
+            if (this.models.Count < this.capacity)
+            {
+                this.models.Add(model);
+            }
+        }
+
+        public bool Exists(string name)
+        {
+            return this.models.Any(x => x.Name == name);
+        }
+
+        public ITeam Get(string name)
+        {
+            return this.models.FirstOrDefault(x => x.Name == name);
+        }
+
+        public bool Remove(string name)
+        {
+            ITeam model = this.models.FirstOrDefault(x => x.Name == name);
+            if (model == null)
+            {
+                return false;
+            }
+            this.models.Remove(model);
+            return true;
+        }
+    }
+}
